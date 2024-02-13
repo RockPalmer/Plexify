c7cc0a66ee8ee0ce60e2f632387c55487759680e
RockPalmer <77813599+RockPalmer@users.noreply.github.com>
2024-02-13 12:46:50 -0700

    Added comments

--- src/string_mapping.h
+++ src/string_mapping.h
@@ -6,6 +6,13 @@
 #define DC_MAP 2
 #define DELIM_MAP 3
 #define INPUT_MAP 4
+/*
+ * This file defines what strings the program will use to write and read
+ * truth table files as well as displaying boolean expressions. This has
+ * been put in this central location rather than hard coded so that the
+ * values can be easily edited should the need arise
+ */
+
 #define OUTPUT_MAP 5
 #define AND_MAP 6
 #define OR_MAP 7
@@ -17,20 +24,20 @@
 #define GROUP_CLOSE_MAP 13
 
 std::string string_mapping[] = {
-	"0",
-	"1",
-	"x",
-	",",
-	"Inputs",
-	"Outputs",
-	" & ",
-	" | ",
-	" ^ ",
-	"~",
-	"?",
-	":",
-	"(",
-	")"
+	"0",		// LOW/false bit
+	"1",		// HIGH/true bit
+	"x",		// don't care bit
+	",",		// truth table file delimiter
+	"Inputs",	// Start of input columns label
+	"Outputs",	// Start of output columns label
+	" & ",		// AND operator
+	" | ",		// OR operator
+	" ^ ",		// XOR operator
+	"~",		// NOT operator
+	"?",		// MUX operator 1
+	":",		// MUX operator 2
+	"(",		// Open grouping symbol
+	")"			// Close grouping symbol
 };
 
 #endif
