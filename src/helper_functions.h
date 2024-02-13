5ca0c030993f0aaf11222e14c38320688c5b93c0
RockPalmer <77813599+RockPalmer@users.noreply.github.com>
2024-02-13 12:41:08 -0700

    Added comments

--- src/helper_functions.h
+++ src/helper_functions.h
@@ -1,11 +1,27 @@
+/* 
+ * This header file defines basic functions that are used throughout 
+ * the program to perform various basic functions 
+ */
+
 #ifndef HELPER_FNS
 #define HELPER_FNS
 
 #include <string>
 
+/* 
+ * This function takes in a string as a parameter and returns a copy
+ * of that string with all the whitespace at the beginning and end of
+ * the string resmoved.
+ */
 std::string trim(std::string str) {
+
+	// Copy the string
 	std::string result = str;
+
+	// If str is not empty
 	if (str.compare("") != 0) {
+
+		// Remove all whitespace from the beginning
 		for (int i = 0; i < result.length(); i++) {
 			if (result[i] != 32 && result[i] != 9) {
 				result = result.substr(i);
@@ -13,6 +29,7 @@ std::string trim(std::string str) {
 			}
 		}
 
+		// Remove all whitespace from the end of the string
 		for (int i = result.length(); i > 0; i--) {
 			if (result[i-1] != 32 && result[i-1] != 9) {
 				result = result.substr(0,i);
@@ -22,6 +39,11 @@ std::string trim(std::string str) {
 	}
 	return result;
 }
+
+/*
+ * Returns a string of binary digits representing v, with <max_bits> bits
+ * If max_bits is -1, use 64 as the maximum bits
+ */
 std::string toBin(size_t v, int max_bits = -1) {
 	size_t w = v;
 	std::string result = "";
