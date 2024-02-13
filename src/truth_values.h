a1105c3e9114b81ff53fcdc9e51ea287f2370ac3
RockPalmer <77813599+RockPalmer@users.noreply.github.com>
2024-02-13 12:47:45 -0700

    Added comments

--- src/truth_values.h
+++ src/truth_values.h
@@ -1,6 +1,9 @@
+/** This file defines global variables that are used throughout the program */
+
 #ifndef TRUTH_VALUES
 #define TRUTH_VALUES
 
+// All available logic gates
 #define NONE 0
 #define NOT 1
 #define AND 2
@@ -11,10 +14,12 @@
 #define XNOR 7
 #define MUX 8
 
+// All possible "truth values" for a given bit
 #define HI '1'
 #define LO '0'
 #define DC 'x'
 
+// All possible types of logical expressions
 enum log_type {
 	LITERAL = 1,
 	VARIABLE = 2,
